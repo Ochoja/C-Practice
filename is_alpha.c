65d1e62491463ade71a2237293f2e63b67b23e67
Daniel Ochoja <danielochoja@yahoo.com>
2022-11-11 19:37:36 +0000

    updated

--- is_alpha.c
+++ is_alpha.c
@@ -2,7 +2,9 @@
 
 int isalpha(char letter)
 {
-	if (letter >= 'a' && letter <= 'Z')
+	if (letter >= 'A' && letter <= 'Z')
+		return (1);
+	else if (letter >= 'a' && letter <= 'z')
 		return (1);
 	else
 		return (0);

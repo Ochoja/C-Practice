bed4d5dcb2974814f6891bdc7c2854ef3c6b8128
Daniel Ochoja <danielochoja@yahoo.com>
2022-11-11 19:43:21 +0000

    added comments

--- is_alpha.c
+++ is_alpha.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/**
+ * isalpha - return 1 if argument is a letter, 0 otherwise
+ * @letter: argument
+ * Return: 0 or 1
+ */
 int isalpha(char letter)
 {
 	if (letter >= 'A' && letter <= 'Z')

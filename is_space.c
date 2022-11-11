225a224d2d0c71844af7c7a320505b2668b82e06
Daniel Ochoja <danielochoja@yahoo.com>
2022-11-11 20:04:15 +0000

    added is_space.c

---
+++ is_space.c
@@ -0,0 +1,14 @@
+#include "headerfile.h"
+
+/**
+ * is_space - returns 1 if argument is space, 0 otherwise
+ * @arg: argument
+ * Return: 0 or 1
+ */
+int is_space (char arg)
+{
+	if (arg == ' ')
+		return (1);
+	else
+		return (0);
+}
